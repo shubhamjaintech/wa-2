d3de1ad258b757e0e9e66c5fc86e60e9ab20e13c
tinkrboy <shubhamjain2796@gmail.com>
2019-12-19 12:15:47 +0530

    cacs version 1 landing page

--- img/style_dynamic.php
+++
@@ -1,1 +0,0 @@
-.content{	margin-top: 90px;}.logo_wrapper,.side_menu_button{	height: 100px;}.content{	margin-top: 130px;}.drop_down .second{	top: 100px;}nav.main_menu > ul > li > a{	line-height: 100px;}nav.main_menu ul li:hover a,nav.main_menu ul li.active a{	color: #2bc253;}.drop_down .second .inner ul > li:hover > a{	color: #2bc253 !important;}
